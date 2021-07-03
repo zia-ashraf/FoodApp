7bc9844c2292ce1f15e36c099f471ad4732b8f65
Zia <zeazzzz4@gmail.com>
2021-07-03 18:15:52 +0530

    using useEffect to animate the cartbutton each time an item is added
    setting a timer to let change the state so that it becomes active for the next change in item

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,17 +1,32 @@
 import React from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
   const ctx = useContext(CartContext); //this header card component will be re-evaluated by react whenever the context changes
   // console.log(ctx.items + " here");
+
+  const [buttonIsHighlighted, setbuttonIsHighlighted] = useState(false);
+
   const numberOfCartItems = ctx.items.reduce((curNum, item) => {
     return curNum + item.amount;
   }, 0);
+
+  useEffect(() => {
+    // console.log("we are inside the useEffect hook");
+    setbuttonIsHighlighted(true);
+    setTimeout(() => {
+      setbuttonIsHighlighted(false);
+    }, 300);
+  }, [ctx.items]);
+
+  const btnClasses = `${classes.button} ${
+    buttonIsHighlighted ? classes.bump : ""
+  }`;
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
